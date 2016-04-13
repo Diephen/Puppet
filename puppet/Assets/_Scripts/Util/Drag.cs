5a81ebec70f343aaa54b49c858f2c277cf6e8e8f
YIWEN DAI <daiyw1992@gmail.com>
2016-04-12 19:52:34 -0700

    add drag code

---
+++ puppet/Assets/_Scripts/Util/Drag.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drag : MonoBehaviour {
+	private Color mouseOverColor = Color.blue;
+	private Color originalColor = Color.yellow;
+	private bool dragging = false;
+	private float distance;
+
+	void OnMouseDown()
+	{
+		distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		dragging = true;
+	}
+
+	void OnMouseUp()
+	{
+		dragging = false;
+	}
+
+	void Update()
+	{
+		if (dragging)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Vector3 rayPoint = ray.GetPoint(distance);
+			transform.position = rayPoint;
+
+		}
+	}
+}
