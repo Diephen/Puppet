94d0e020894954f034d377d66c6eb053db363dd9
jsohn96 <jsohn96@hotmail.com>
2016-04-23 16:53:10 -0700

    Commit Materials

---
+++ strawdog/Assets/_Scripts/5.PoopStory/Actor_Girl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor_Girl : MonoBehaviour {
+
+    Actors _thisActor;
+
+    void Awake () {
+        _thisActor = gameObject.GetComponent <Actor> ()._thisActor;
+    }
+
+    void OnEnable () {
+        Events.G.AddListener<PoopStoryAct>(ActFunction);
+    }
+
+    void OnDisable () {
+        Events.G.RemoveListener<PoopStoryAct>(ActFunction);
+    }
+
+    public void ActFunction (PoopStoryAct e) {
+        //        if (_thisActor == e.Actors){
+        //            Debug.Log ("actor " + _thisActor);
+        //        } else if (_thisActor == e.Victims) {
+        //            Debug.Log ("victim" + _thisActor);
+        //        } else {
+        //            Debug.Log ("static" + _thisActor);
+        //        }
+        //        e.GirlState;
+        //        e.DoorState;
+        //        e.ChainState;
+        //        e.PoopMonsterState;
+        //        e.ShadowState;
+        //        e.PoopParticleState;
+    }
+}
