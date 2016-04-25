2c3bcad084c35836f3f3da6b4c0e645afc3ed939
jsohn96 <jsohn96@hotmail.com>
2016-04-24 22:02:49 -0700

    Rotation

--- strawdog/Assets/_Scripts/5.PoopStory/Actor_Lights.cs
+++ strawdog/Assets/_Scripts/5.PoopStory/Actor_Lights.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class Actor_Lights : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    Actors _thisActor;
+    LightsPoop _priorState;
+
+    void Awake () {
+        _thisActor = gameObject.GetComponent <Actor> ()._thisActor;
+    }
+
+    void OnEnable () {
+        Events.G.AddListener<PoopStoryAct>(ActFunction);
+    }
+
+    void OnDisable () {
+        Events.G.RemoveListener<PoopStoryAct>(ActFunction);
+    }
+
+    public void ActFunction (PoopStoryAct e) {
+        switch (e.LightsPoopState) {
+        case LightsPoop.center:
+            Debug.Log ("closed");
+            break;
+        case LightsPoop.side:
+            Debug.Log ("locked0");
+            break;
+        }
+        _priorState = e.LightsPoopState;
+
+    }
+
+
+    void OnMouseDown(){
+        Vector3 mouseDelta = Input.mousePosition;
+
+        Vector3 worldMouse = Camera.main.ScreenToWorldPoint (mouseDelta);
+
+        float angle = Mathf.Atan2 (worldMouse.y - transform.position.y, worldMouse.x - transform.position.x) * Mathf.Rad2Deg;
+        if (angle < 0) { 
+            angle += 360;
+        }
+
+        Debug.Log (angle);
+
+        transform.localEulerAngles = new Vector3 
+            (transform.localEulerAngles.x,
+                transform.localEulerAngles.y, 
+                angle+90);
+    
+    }
 }
