0cf47b6035dd2493025cc824e5cba33efaff785d
jsohn96 <jsohn96@hotmail.com>
2016-04-28 11:23:07 -0700

    Collision and Floor Fix

--- strawdog/Assets/_Scripts/Drag.cs
+++ strawdog/Assets/_Scripts/Drag.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Drag : MonoBehaviour {
     bool dragging = false;
@@ -8,6 +9,7 @@ public class Drag : MonoBehaviour {
     Actors draggedVictim;
     GameObject _draggedVictimGameObject;
 
+    List<GameObject> _hover = new List<GameObject> ();
 
     void Awake() {
         _draggedActor = gameObject.GetComponent<Actor> ()._thisActor;
@@ -33,7 +35,9 @@ public class Drag : MonoBehaviour {
     }
 
     void CheckCollision() {
-        
+        if (_hover.Count != 0) {
+            _draggedVictimGameObject = _hover [0];
+        }
         //check if dropped on actor
         if (_draggedVictimGameObject != null && _draggedVictimGameObject.GetComponent <Actor> () != null) {    
             
@@ -45,6 +49,7 @@ public class Drag : MonoBehaviour {
             _draggedVictimGameObject.GetComponent <Actor_Shadow> ().ThisShadow ();
         }
         Debug.Log ("Act: " + _draggedActor+", Vict: " + draggedVictim);
+        _hover.Clear ();
         Events.G.Raise(new DirectorUpdate(_draggedActor, draggedVictim));
     }
 
@@ -54,13 +59,24 @@ public class Drag : MonoBehaviour {
 			if (other.CompareTag("Actor")) {
 //				Debug.Log (other.name);
 				_draggedVictimGameObject = other.gameObject;
+                _hover.Add(_draggedVictimGameObject);
 			}
+            Debug.Log ("ENTER "+_hover);
         }
     }
 
     void OnTriggerExit2D(Collider2D other) {
-		Debug.Log (other.name);
-        _draggedVictimGameObject = null;
+//		Debug.Log (other.name);
+        if (dragging) {
+            if (other.CompareTag ("Actor")) {
+                Debug.Log (_hover.Count);
+                _draggedVictimGameObject = null;
+                if (_hover.Count != 0) {
+                    _hover.RemoveAt (_hover.Count - 1);
+                }
+                Debug.Log ("Exit: " + _hover);
+            }
+        }
     }
 
 
