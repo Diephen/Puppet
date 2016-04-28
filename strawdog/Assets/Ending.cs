14590eee733ef4327a8f413618a9912c666358c5
jsohn96 <jsohn96@hotmail.com>
2016-04-28 01:04:36 -0700

    Endings

---
+++ strawdog/Assets/Ending.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ending : MonoBehaviour {
+
+    [SerializeField] Sprite _ending1;
+    [SerializeField] Sprite _ending2;
+    [SerializeField] Sprite _ending3;
+    SpriteRenderer _spriteRenderer;
+
+    Fading _fading;
+
+    void Start(){
+        _spriteRenderer = gameObject.GetComponent <SpriteRenderer> ();
+        _fading = gameObject.GetComponent <Fading> ();
+        _spriteRenderer.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void EndingReveal(int end) {
+        if(end == 1){
+            _spriteRenderer.sprite = _ending1;
+        } else if(end == 2){
+            _spriteRenderer.sprite = _ending2;
+        } else if(end == 3) {
+            _spriteRenderer.sprite = _ending3;
+        }
+        StartCoroutine (WaitForEnd());
+    }
+
+
+
+    IEnumerator WaitForEnd(){
+        yield return new WaitForSeconds(.5f);
+        _fading.BeginFade(1);
+        yield return new WaitForSeconds(2f);
+        _spriteRenderer.enabled = true;
+        _fading.BeginFade(-1);
+        //        Application.LoadLevel (2);
+    }
+}
