8c2002ff474d1e947b4cfb071448c7959511da5c
jsohn96 <jsohn96@hotmail.com>
2016-04-28 01:24:10 -0700

    Girl Pass Our Animation

--- strawdog/Assets/Ending.cs
+++ strawdog/Assets/Ending.cs
@@ -28,6 +28,7 @@ public class Ending : MonoBehaviour {
             _spriteRenderer.sprite = _ending1;
         } else if(end == 2){
             _spriteRenderer.sprite = _ending2;
+            _girlAnimator.SetTrigger ("triggerEnd");
         } else if(end == 3) {
             _spriteRenderer.sprite = _ending3;
         }
@@ -37,7 +38,7 @@ public class Ending : MonoBehaviour {
 
 
     IEnumerator WaitForEnd(){
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(3f);
         _fading.BeginFade(1);
         yield return new WaitForSeconds(2f);
         _spriteRenderer.enabled = true;
